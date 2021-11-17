89685a1ead1f858e1d6cb13ec7ef2f1078f9cd89
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-11-16 22:05:02 -0300

    03.Comments

---
+++ JS - 02 - JavaScript Básico - Iniciantes/comment.js
@@ -0,0 +1,10 @@
+// Write a comment
+console.log('Hello World'); // Here's another comment
+console.log('Line 1');
+console.log('Line 2');
+console.log('Line 3');
+console.log('Line 4');
+// Will be ignored
+/**
+ * Multiple comment
+ */
