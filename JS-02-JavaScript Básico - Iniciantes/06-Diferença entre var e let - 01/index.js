34ca0c986315467adf249981a79f3337dd90d4a6
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-11-22 18:03:36 -0300

    06.Diferença entre var e let

---
+++ JS-02-JavaScript Básico - Iniciantes/06-Diferença entre var e let - 01/index.js
@@ -0,0 +1,8 @@
+//REDECLARAÇÃO: Com var é possível declarar a variavel mais de uma vez
+var firstName = 'Vicente';
+var firstName = 'Freitas';
+console.log(firstName);
+
+//REDECLARAÇÃO: Com let só se declara a variável uma única vez
+let lastName = 'Freitas';
+// let lastName = 'T Freitas';
