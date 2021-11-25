0fb8b55f8dc93c89dc7d78f7048cee3dffb470c4
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2021-11-25 18:20:32 -0300

    Exercicio - Alert, confirme prompt - Browser

---
+++ JS-02-JavaScript Básico - Iniciantes/09-Alert confirm e prompt - Browser/js/script.js
@@ -0,0 +1,13 @@
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+// const soma = num1 + num2;
+
+alert(`Soma dos números digitados é = ${num1 + num2}`);
+
+console.log(`Número 1:  ${num1}`);
+console.log(`Número 2:  ${num2}`);
+console.log(`Soma = ${num1 + num2}`);
